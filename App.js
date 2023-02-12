1ac8042aaa0568afbc656bc0386539da57a9b5e2
Pratikesh Kumar <122167622+Pratikesh-kumar@users.noreply.github.com>
2023-02-12 13:01:19 +0530

    update by map

--- App.js
+++ App.js
@@ -7,56 +7,46 @@ function App() {
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      
+      place : 'Patna',
     },
     {
        id: 'e2', 
     title: 'New TV',
      amount: 799.49,
-      date: new Date(2021, 2, 12),
-
+      date: new Date(2021, 2, 12), 
+      place : 'Patna',
     },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
+      place : 'Patna',
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
+      place : 'Patna',
     },
   ];
   return (
     <div>
       <h2> Expense Tracker</h2>
-      <ExpenseItem1
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}>
-      </ExpenseItem1>
+     {expenses.map((Val) => {
+                return (
+                    <div>
+                        <ExpenseItem1 title={Val.title}
+                            amount={Val.amount}
+                            date={Val.date}
+                            place={Val.place}>
+                             </ExpenseItem1>
+                    </div>
+                )
+            })}
       
-      <ExpenseItem1
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}>
-      </ExpenseItem1>
-
-      <ExpenseItem1
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}>
-      </ExpenseItem1>
-
-      <ExpenseItem1
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}>
-      </ExpenseItem1>
-      
-
+            
 
       
     </div>
