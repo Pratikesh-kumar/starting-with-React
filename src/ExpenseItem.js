9c744d88d0c89f428fd43ab43ea598a2e7324485
Pratikesh-kumar <pratikeshkumar96@gmail.com>
2023-02-12 12:56:13 +0530

    commit map

---
+++ src/ExpenseItem.js
@@ -0,0 +1,13 @@
+
+function ExpenseItem() {
+  return(
+  <div>
+            <h2>Expenseitem!</h2>
+            <h3>food - 100  </h3> 
+            <h3>petrol- 100</h3>
+            <h3>movies - 200</h3>
+
+</div>
+  )
+}
+ export default ExpenseItem;
