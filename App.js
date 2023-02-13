c25a796a7e8e0d113c5ddbab5f66cbfa7351ebc3
Pratikesh Kumar <122167622+Pratikesh-kumar@users.noreply.github.com>
2023-02-13 13:37:40 +0530

    main App

---
+++ App.js
@@ -0,0 +1,12 @@
+import Expense from './components/Expenses';
+function App() {
+  
+  return (
+    <div>
+      <h2> Expense Tracker</h2>
+      <Expense></Expense>
+     </div>
+  )
+  }
+
+export default App;
