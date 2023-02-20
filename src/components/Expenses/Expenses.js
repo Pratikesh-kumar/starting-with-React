95a51fbb5191e02379f55c5f5cc3266a4c4e5786
Pratikesh-kumar <pratikeshkumar96@gmail.com>
2023-02-20 22:31:05 +0530

    the edited final files

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,67 +1,34 @@
-import React,{useState} from "react";
-import ExpenseItem1 from "./ExpenseItem1";
-import './Expenses.css';
-import Card from "../UI/Card";
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
 
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-function Expense(props) {
-    const [filteredYear, setFilteredYear] = useState('2020');
-
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            place: 'PATNA',
-            amount: 900,
-            date: new Date(2020, 7, 14),
-        },
-        { id: 'e2',
-         title: 'New TV',
-          place: 'PATNA',
-           amount: 800, 
-           date: new Date(2021, 2, 12)
-         },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            place: 'PATNA',
-            amount: 300,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            place: 'PATNA',
-            amount: 500,
-            date: new Date(2021, 5, 12),
-        }
-    ];
-    return (
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-        <Card className='expenses'>
-            
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
 
-            {expenses.map((Val) => {
-                return (
-                    <div>
-                        <ExpenseItem1 id={Val.id}
-                        title={Val.title}
-                            amount={Val.amount}
-                            date={Val.date}
-                            place={Val.place}> </ExpenseItem1>
-                            
-                    </div>
-                )
-            })}
-        </Card>
-    )
-
-}
-
-export default Expense;
+export default Expenses;
